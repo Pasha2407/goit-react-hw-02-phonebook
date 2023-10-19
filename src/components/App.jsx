0066dba41ee054c6744d067ed4a13a39b339defe
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2023-10-20 02:15:37 +0300

    phonebook completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+import css from './ContactForm.module.css';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: '1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: '2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: '3', name: 'Eden Clements', number: '645-17-79' },
+    ],
+    filter: '',
+  };
+
+  AddContact = contactData => {
+    const hasDuplicates = this.state.contacts.some(
+      item => item.name === contactData.name
+    );
+    if (hasDuplicates) {
+      alert(`${contactData.name} is already in contacts.`);
+      return;
+    } else {
+      const finalProduct = {
+        ...contactData,
+        id: nanoid(),
+      };
+      this.setState({
+        contacts: [...this.state.contacts, finalProduct],
+      });
+    }
+  };
+
+  DeleteContact = contactId => {
+    this.setState({
+      contacts: this.state.contacts.filter(item => item.id !== contactId),
+    });
+  };
+
+  InputFilterChange = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  FilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const filter = this.FilteredContacts();
+    return (
+      <div className={css.main}>
+        <h2>Phonebook</h2>
+        <ContactForm addContact={this.AddContact} />
+        <h2>Contacts</h2>
+        <Filter
+          filterValue={this.state.filter}
+          filterChange={this.InputFilterChange}
+        />
+        <ContactList contacts={filter} deleteContact={this.DeleteContact} />
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,48 @@
+import { Component } from 'react';
+import css from './ContactForm.module.css';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  InputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({ [name]: value });
+  };
+
+  Submit = event => {
+    event.preventDefault();
+    const contactData = {
+      name: this.state.name,
+      number: this.state.number,
+    };
+    this.props.addContact(contactData);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.Submit} className={css.form}>
+        <label>
+          <span>Name</span>
+          <input
+            type="text"
+            name="name"
+            onChange={this.InputChange}
+            value={this.state.name}
+          />
+          <span>Number</span>
+          <input
+            type="text"
+            name="number"
+            onChange={this.InputChange}
+            value={this.state.number}
+          />
+        </label>
+        <button type="submit">Add contact </button>
+      </form>
+    );
+  }
+}
